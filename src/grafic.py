e1b5752ba9158826df54111c9a8fb13ba90795ba
parasite-kremato <fert1sed@gmail.com>
2025-03-31 14:57:24 +0300

    Update grafic.py

--- src/grafic.py
+++ src/grafic.py
@@ -4,21 +4,21 @@ from math import pow
 # Функция для вычисления y
 def f(x):
     return (pow(x, 2)) / 2 - 2
-def draw_graf(width, canvas, height):
+def draw_graph(canvas, width, height):
     canvas.delete("all")
     x_min, x_max = -20, 20
     y_min, y_max = -20, 20
 
-    x_scale = width (x_max - x_min)
-    y_scale = height (y_max - y_min)
+    x_scale = width / (x_max - x_min)
+    y_scale = height / (y_max - y_min)
 
     # Оси
     canvas.create_line(0, height / 2, width, height / 2, fill="black", arrow=tk.LAST)  # Ось x
     canvas.create_line(width / 2, height, width / 2, 0, fill="black", arrow=tk.LAST) # Ось y
 
-    canvas.create_text(width / 2 + 10, height / 2 - 10, text = "0", ancho=tk.W)
-    canvas.create_text(width / 2 + 10, 10, text = "y", anchor=tk.W)
-    canvas.create_text(width - 10, height / 2 + 10, text = "x", anchor=tk.E)
+    canvas.create_text(width / 2 + 10, height / 2 - 10, text ="0", ancho=tk.W)
+    canvas.create_text(width / 2 + 10, 10, text ="y", anchor=tk.W)
+    canvas.create_text(width - 10, height / 2 + 10, text ="x", anchor=tk.E)
 
     canvas.create_oval(width / 2 - 3, height / 2 - 3, width / 2 + 3, height / 2 + 3, fill="white") # Точка О
 
@@ -33,4 +33,14 @@ def draw_graf(width, canvas, height):
             (x2 - x_min) * x_scale, height - (y2 - y_min) * y_scale,
             fill="blue"
         )
+
+root = tk.Tk()
+root.title("График функции y = x^2/2 - 2")
+
+canvas = tk.Canvas(root, width=800, height=600, bg="white")
+canvas.pack()
+
+draw_graph(canvas, 800, 600)
+root.mainloop()
+
 
